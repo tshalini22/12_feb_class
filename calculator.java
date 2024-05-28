31e437e3eeebaaeed381e371e512ee8835722388
tshalini22 <tshalini22@gmail.com>
2024-05-28 13:13:53 +0530

    added a cal support

---
+++ calculator.java
@@ -0,0 +1,11 @@
+class calculator{
+    private int ans;
+    public int add(int a){
+        ans+=a;
+        return ans;
+    }
+    public int sub(int b){
+        ans-=b;
+        return ans;
+    }
+}
